1d647217641023938d1555412b39f46692886694
Udit Prabhu <uditprabhu@gmail.com>
2026-08-11 19:19:13 +0530

    Update everywhere to new publishable key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://gsmaxkxooaverirtkdot.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzbWF4a3hvb2F2ZXJpcnRrZG90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNjQ4NDgsImV4cCI6MjEwMTk0MDg0OH0.ceGMTeFAQct_dd2f_jNhHzEpzDUR8J23OniSD5yANO4";
+const supabaseKey = "sb_publishable_y4BOehQuKshgos7R69hFYg_SKumk-Zl";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
